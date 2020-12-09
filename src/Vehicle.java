0849fe227a2f0740a1ee3cc4cc3a3dcf5563eab4
Falserok <71407741+Falserok@users.noreply.github.com>
2020-12-09 20:44:09 +0300

    dz3.5

---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public interface Vehicle<T> {
+    float checkFuelCapacity();
+    void go();
+    float getFuel();
+    float getFuelConsumption();
+    String getName();
+    float getTankCapacity();
+    void setFuel(float fuel);
+}
